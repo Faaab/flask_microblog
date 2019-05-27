1d959f502679141b4377ba03a04f8da76f662f81
Faaab <35370400+Faaab@users.noreply.github.com>
2019-05-27 13:20:55 +0200

    Add custom error pages for 404 and 500

--- app/errors.py
+++ app/errors.py
@@ -1,8 +1,8 @@
 from flask import render_template
-drom app import app, db
+from app import app, db
 
 @app.errorhandler(404)
-def not_found_error(404):
+def not_found_error(error):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
