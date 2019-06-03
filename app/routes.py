0dabb02f7647533f564d9d8bc29c1e7c8986c582
Fabian <fabian.vandijk@ogd.nl>
2019-06-03 10:58:34 +0200

    Add pagination to user posts

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('index', page=posts.next_num) \
-        if post.has_next else None
+        if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title = "Home", form=form, posts=posts.items,
@@ -101,12 +101,15 @@ def explore():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'},
-        {'author': user, 'body': 'Test post #3'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                            next_url=next_url, prev_url=prev_url)
 
 @app.route('/follow/<username>')
 @login_required
