6181f7c8bd4f5903eabe8a7964078a1995fa6b68
Fabian <fabian.vandijk@ogd.nl>
2019-06-04 09:39:13 +0200

    Add Babel locale selector

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel
+from flask_babel import lazy_gettext as _l
 
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -51,3 +53,7 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
