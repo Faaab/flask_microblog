6be00bafafdd5c08aed0797d6e8b0f6d81f5bb31
Fabian <fabian.vandijk@ogd.nl>
2019-06-04 17:02:25 +0200

    Edit all calls to url_for to reference new location of routes.py in auth blueprint

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -33,13 +33,13 @@ def index():
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
 
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) \
+    next_url = url_for('auth.index', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
+    prev_url = url_for('auth.index', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title = "Home", form=form, posts=posts.items,
                             next_url=next_url, prev_url=prev_url)
@@ -47,30 +47,30 @@ def index():
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     form = LoginForm()
     # for.validate_on_submit() returns True if a form was submitted
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash(_('Invalid username or password'))
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('auth.index')
         return redirect(next_page)
     return render_template('login.html', title="Sign In", form=form)
 
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('auth.index'))
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -78,35 +78,35 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash(_('Congratulations, you are now a registered user!'))
-        return redirect(url_for('login'))
+        return redirect(url_for('auth.login'))
     return render_template('register.html', title='Register', form=form)
 
 @bp.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             send_password_reset_email(user)
         flash(_('Check your email for the instructions to reset your password'))
-        return redirect(url_for('login'))
+        return redirect(url_for('auth.login'))
     return render_template('reset_password_request.html', title='Reset Password', form=form)
 
 @bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     user = User.verify_reset_password_token(token)
     if not user:
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
         flash(_('Your password has been reset.'))
-        return redirect(url_for('login'))
+        return redirect(url_for('auth.login'))
     return render_template('reset_password.html', form=form)
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -118,7 +118,7 @@ def edit_profile():
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash(_('Your changes have been saved'))
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('auth.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
@@ -129,9 +129,9 @@ def edit_profile():
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('explore', page=posts.next_num) \
+    next_url = url_for('auth.explore', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) \
+    prev_url = url_for('auth.explore', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Explore', posts=posts.items,
                             next_url=next_url, prev_url=prev_url)
@@ -143,9 +143,9 @@ def user(username):
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
+    next_url = url_for('auth.user', username=user.username, page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+    prev_url = url_for('auth.user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
                             next_url=next_url, prev_url=prev_url)
@@ -156,14 +156,14 @@ def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     if user == current_user:
         flash('You cannot follow yourself!')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('auth.user', username=username))
     current_user.follow(user)
     db.session.commit()
     flash(_('You are following %(username)s!', username=username))
-    return redirect(url_for('user', username=username))
+    return redirect(url_for('auth.user', username=username))
 
 @bp.route('/unfollow/<username>')
 @login_required
@@ -171,14 +171,14 @@ def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash(_('User %(username)s not found.', username=username))
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.index'))
     if user == current_user:
         flash(_('You cannot unfollow yourself!'))
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('auth.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
     flash(_('You are not following %(username)s.', username=username))
-    return redirect(url_for('user', username=username))
+    return redirect(url_for('auth.user', username=username))
 
 @bp.route('/translate', methods=['POST'])
 @login_required
