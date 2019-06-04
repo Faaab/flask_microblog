cb8710aa13f4d0ac5775f9e2edd9ab002b7347d9
Fabian <fabian.vandijk@ogd.nl>
2019-06-04 13:11:15 +0200

    Add translate route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.email import send_password_reset_email
+from app.translate import translate
 from flask_login import current_user, login_user, login_required, logout_user
 from flask_babel import _, get_locale
 from app.models import User, Post
@@ -175,3 +176,10 @@ def unfollow(username):
     db.session.commit()
     flash(_('You are not following %(username)s.', username=username))
     return redirect(url_for('user', username=username))
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
