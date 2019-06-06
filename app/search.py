bc15357af4ea91a3e8de0dcee0d8b341176461cf
Fabian <fabian.vandijk@ogd.nl>
2019-06-06 13:50:43 +0200

    Delete deprecated parameter to elasticsearch.search

--- app/search.py
+++ app/search.py
@@ -18,7 +18,6 @@ def query_index(index, query, page, per_page):
         return [], 0
     search = current_app.elasticsearch.search(
         index=index,
-        doc_type=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
               'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
