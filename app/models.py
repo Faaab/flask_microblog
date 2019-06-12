0187937b62a8df2a344a25c3bd966035ac65a5d8
Fabian <fabian.vandijk@ogd.nl>
2019-06-12 09:20:19 +0200

    Add Message model

--- app/models.py
+++ app/models.py
@@ -128,6 +128,16 @@ class Post(db.Model, SearchableMixin):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.Datetime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
