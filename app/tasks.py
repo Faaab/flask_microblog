8501394bc51c1b5061f3452cee8fd6093141fa7b
Fabian <fabian.vandijk@ogd.nl>
2019-06-13 09:18:35 +0200

    Replace test task with _set_task_progress

--- app/tasks.py
+++ app/tasks.py
@@ -1,14 +1,18 @@
-import time
+from app import create_app, db
+from app.models import Task
 from rq import get_current_job
 
-def example(seconds):
-    job = get_current_job()
-    print("Starting task")
-    for i in range(seconds):
-        job.meta['progress'] = 100.0 * i / seconds
+app = create_app
+app.app_context().push()
+
+def _set_task_progress(progress):
+    job = get_current_job():
+    if job:
+        job.meta['progress'] = progress
         job.save_meta()
-        print(i)
-        time.sleep(1)
-    job.meta['progress'] = 100
-    job.save_meta()
-    print("Task completed")
+        task = Task.query.get(job.get_id())
+        task.user.add_notification('task_progress', {'task_id': job.get_id(),
+                                                     'progress': progress})
+        if progress >= 100:
+            task.complete = True
+        db.session.commit()
