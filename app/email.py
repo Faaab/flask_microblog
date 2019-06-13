75076b370757becfd0e5a4676c234c46f21f2caa
Fabian <fabian.vandijk@ogd.nl>
2019-06-13 15:44:50 +0200

    make first param of call to Message-constructor explicit

--- app/email.py
+++ app/email.py
@@ -9,7 +9,7 @@ def send_async_email(app, msg):
 
 def send_email(subject, sender, recipients, text_body, html_body,
                attachments=None, sync=False):
-    msg = Message(subject, sender=sender, recipients=recipients)
+    msg = Message(subject=subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     if attachments:
